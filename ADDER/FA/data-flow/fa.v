567b52f0f01c81ff619cf4bbdd435828cc001ae2
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-13 12:11:59 +0330

    feat: full adder using data flow modeling

---
+++ ADDER/FA/data-flow/fa.v
@@ -0,0 +1,14 @@
+module full_adder (
+    input a_i, b_i, cin,
+    output sum_o, carry_o
+);
+    wire xor_res, first_and, second_and;
+    
+    assign xor_res = a_i ^ b_i;
+    assign first_and = a_i & b_i;
+    assign second_and = xor_res & cin;
+
+    assign sum_o = xor_res ^ cin;
+    assign carry_o = first_and | second_and;
+
+endmodule
